19099e03c3dacbeda356c66048932c20519413cc
Anbu <15264938+anburocky3@users.noreply.github.com>
2022-08-12 19:05:50 +0530

    TailwindCLI & ReactJS (Till our lesson)

---
+++ fuel-cost-calculator/src/App.js
@@ -0,0 +1,51 @@
+
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  // const [tripDistance] = useState(0)
+  // const [fuelConsumption] = useState(0)
+  // const [costPerLitre] = useState(0)
+
+  const tripDistance = 120;
+  const fuelConsumption = 12;
+  const costPerLitre = 102;
+
+  const totalLitreConsumed = tripDistance / fuelConsumption
+  const totalCost = totalLitreConsumed * costPerLitre
+  alert(totalCost);
+
+  // totalLitreConsumed = distance / consumption
+
+  // totalCost of litre = totalLitreConsumed * cost of litre 
+  return (
+    <div className='h-screen bg-indigo-500 p-10'>
+      <div className='bg-white p-5 rounded-lg max-w-lg mx-auto'>
+        <div>
+          <h1 className='text-xl font-semibold'>Fuel Cost Calculator</h1>
+          <p className='text-gray-600'>Know your expense</p>
+        </div>
+        <div className='mt-5 space-y-3'>
+          <div className='flex justify-between items-center'>
+            <p>Trip Distance (KM)</p>
+            <input type="number" placeholder='Trip Distance' className='border  px-4 py-2 rounded' value={tripDistance} />
+          </div>
+          <div className='flex justify-between items-center'>
+            <p>Fuel Consumption</p>
+            <input type="number" placeholder='Fuel Consumption' className='border  px-4 py-2 rounded' />
+          </div>
+          <div className='flex justify-between items-center'>
+            <p>Cost Per Litre (L)</p>
+            <input type="number" placeholder='Cost Per Litre' className='border  px-4 py-2 rounded' />
+          </div>
+          <div className='text-center'>
+            <button className='px-4 py-2 rounded'>Reset</button>
+            <button className='bg-indigo-600 px-4 py-2 rounded text-white'>Calculate</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
